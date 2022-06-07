2ff806831e1a39db82bf25e72d8f79d04ceb9508
Aleksander Pawłowski <apawlowski@fly.pl>
2022-06-07 17:57:15 +0200

    change components to function components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,17 +1,14 @@
-import { Component } from "react";
-
-
 import ListElement from "../list-element/list-element.component";
-import './card-list.styles.css';
+import "./card-list.styles.css";
 
-export default class CardList extends Component {
-    render() {
-        const {monsters} = this.props;
-        return (
+const CardList = ({ monsters }) => {
+    return (
         <div className="card-list">
-            {monsters.map(monster => {
-                return <ListElement key={monster.id} monster={monster}/>
+            {monsters.map((monster) => {
+                return <ListElement key={monster.id} monster={monster} />;
             })}
-        </div>);
-    }
-}
+        </div>
+    );
+};
+
+export default CardList;
--- src/components/list-element/list-element.component.jsx
+++ src/components/list-element/list-element.component.jsx
@@ -1,17 +1,20 @@
-import { Component } from "react";
+const ListElement = ({ 
+        monster: {
+            name, 
+            email, 
+            id
+        } 
+    }) => {
+    return (
+        <div key={id} className="card-container">
+            <img
+                alt={`monster ${name}`}
+                src={`https://robohash.org/${id}/?set=2`}
+            />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    );
+};
 
-export default class ListElement extends Component {
-    render() {
-        const { name, email, id } = this.props.monster;
-        return (
-            <div key={id} className="card-container">
-                <img
-                    alt={`monster ${name}`}
-                    src={`https://robohash.org/${id}/?set=2`}
-                />
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        );
-    }
-}
+export default ListElement;
